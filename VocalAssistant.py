de900301aacff85aae19c3b6d68dfbdc4cf06de4
mdeliso97 <marco.deliso@students.unibe.ch>
2023-04-24 09:43:36 +0200

    Minor correction to the vocal assistant, now it tells whether we raised right or left hand

--- VocalAssistant.py
+++ VocalAssistant.py
@@ -1,6 +1,10 @@
 import pyttsx3
 
 
+# Takes track of the previous vocal output
+count_left = False
+count_right = False
+
 # Initialize the pyttsx3 engine
 engine = pyttsx3.init()
 
@@ -11,21 +15,27 @@ engine.setProperty('voice', voices[1].id)  # Set the voice to the second voice i
 
 def command_respond(results):
 
-    # Loop through each detected hand
-    for hand_idx, hand_landmarks in enumerate(results.multi_hand_landmarks):
+    global count_right, count_left
+    if results.multi_hand_landmarks:
+        # Loop through each detected hand
+        for hand_idx, hand_landmarks in enumerate(results.multi_hand_landmarks):
 
-        # Get the handness of the detected hand
-        handness = results.multi_handedness[hand_idx].classification[0].label
+            # Get the handness of the detected hand
+            handness = results.multi_handedness[hand_idx].classification[0].label
 
-        # Determine if the detected hand is a left or right hand
-        if handness == 'Left':
-            # Detected hand is a left hand
-            text = "Left Hand"
-            engine.say(text)
-            engine.runAndWait()
+            # Determine if the detected hand is a left or right hand
+            if handness == 'Left' and not count_right:
+                # Detected hand is a right hand, axes inverted mirrored image
+                text = "Selected Right Hand"
+                engine.say(text)
+                engine.runAndWait()
+                count_right = True
+                count_left = False
 
-        elif handness == 'Right':
-            # Detected hand is a right hand
-            text = "Right Hand"
-            engine.say(text)
-            engine.runAndWait()
+            elif handness == 'Right' and not count_left:
+                # Detected hand is a left hand, axes inverted mirrored image
+                text = "Selected Left Hand"
+                engine.say(text)
+                engine.runAndWait()
+                count_left = True
+                count_right = False
