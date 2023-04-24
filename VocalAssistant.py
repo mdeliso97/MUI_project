65ce648aaa1240b027bb75aa49b99ee09a470457
mdeliso97 <marco.deliso@students.unibe.ch>
2023-04-24 09:01:44 +0200

    Works but needs to be multi threading

--- VocalAssistant.py
+++ VocalAssistant.py
@@ -1,7 +1,4 @@
-import cv2
 import pyttsx3
-import mediapipe as mp
-from mediapipe.python.solutions import hands
 
 
 # Initialize the pyttsx3 engine
@@ -11,40 +8,24 @@ engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)  # Set the voice to the second voice in the list
 
-# Convert text to speech
-text = "Hello, how are you?"
-engine.say(text)
-engine.runAndWait()
 
+def command_respond(results):
 
-def command_respond(image, hands):
-    # Get output image from Mediapipe main class captured from webcam
-    # Hands is the Handtracking output that Mediapipe produces
+    # Loop through each detected hand
+    for hand_idx, hand_landmarks in enumerate(results.multi_hand_landmarks):
 
-    # Convert the input image to RGB
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # Get the handness of the detected hand
+        handness = results.multi_handedness[hand_idx].classification[0].label
 
-    # Run hand tracking on the input image
-    results = hands.process(image)
-
-    # Check if any hands were detected
-    if results.multi_hand_landmarks:
-        # Loop through each detected hand
-        for hand_idx, hand_landmarks in enumerate(results.multi_hand_landmarks):
-
-            # Get the handness of the detected hand
-            handness = results.multi_handedness[hand_idx].classification[0].label
-
-            # Determine if the detected hand is a left or right hand
-            if handness == 'Left':
-                text = "Left Hand"
-                engine.say(text)
-                engine.runAndWait()
+        # Determine if the detected hand is a left or right hand
+        if handness == 'Left':
             # Detected hand is a left hand
-            # Do something with the left hand landmarks
-            elif handness == 'Right':
-                text = "Left Hand"
-                engine.say(text)
-                engine.runAndWait()
-        # Detected hand is a right hand
-        # Do something with the right hand landmarks
+            text = "Left Hand"
+            engine.say(text)
+            engine.runAndWait()
+
+        elif handness == 'Right':
+            # Detected hand is a right hand
+            text = "Right Hand"
+            engine.say(text)
+            engine.runAndWait()
