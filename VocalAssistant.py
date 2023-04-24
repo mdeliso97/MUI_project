d52f1632907a9534ba68196c185e3ee7df582e18
mdeliso97 <marco.deliso@students.unibe.ch>
2023-04-24 09:45:09 +0200

    Still a bit buggy, but now runs concurrently, it states sometimes multiple times which hand we selected, need to explore this bug

--- VocalAssistant.py
+++ VocalAssistant.py
@@ -1,9 +1,7 @@
 import pyttsx3
 
-
 # Takes track of the previous vocal output
-count_left = False
-count_right = False
+vocal_output = False
 
 # Initialize the pyttsx3 engine
 engine = pyttsx3.init()
@@ -14,8 +12,9 @@ engine.setProperty('voice', voices[1].id)  # Set the voice to the second voice i
 
 
 def command_respond(results):
+    # Takes track of the previous vocal output
+    global vocal_output
 
-    global count_right, count_left
     if results.multi_hand_landmarks:
         # Loop through each detected hand
         for hand_idx, hand_landmarks in enumerate(results.multi_hand_landmarks):
@@ -24,18 +23,17 @@ def command_respond(results):
             handness = results.multi_handedness[hand_idx].classification[0].label
 
             # Determine if the detected hand is a left or right hand
-            if handness == 'Left' and not count_right:
+            if handness == 'Left' and not vocal_output:
                 # Detected hand is a right hand, axes inverted mirrored image
                 text = "Selected Right Hand"
                 engine.say(text)
                 engine.runAndWait()
-                count_right = True
-                count_left = False
+                vocal_output = True
 
-            elif handness == 'Right' and not count_left:
-                # Detected hand is a left hand, axes inverted mirrored image
-                text = "Selected Left Hand"
-                engine.say(text)
-                engine.runAndWait()
-                count_left = True
-                count_right = False
+            elif handness == 'Right':
+                if vocal_output:
+                    # Detected hand is a left hand, axes inverted mirrored image
+                    text = "Selected Left Hand"
+                    engine.say(text)
+                    engine.runAndWait()
+                    vocal_output = False
